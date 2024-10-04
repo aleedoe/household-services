d19fcb61f5c3afd7a2e9ef64dbaaaf207ab88ab1
alie <mushyafaali@gmail.com>
2024-10-04 16:43:49 +0700

    update admin view

--- app/views/admin.py
+++ app/views/admin.py
@@ -2,6 +2,18 @@ from app.views import apiBlueprint
 from app.controllers.admin import *
 
 
-@apiBlueprint.route('/api/admins', methods=['GET'])
-def get_admins():
-    return controller_get_admins()
+@apiBlueprint.route('/api/admins', methods=['GET', 'POST'])
+def manage_admins():
+    if request.method == 'GET':
+        return controller_get_admins()
+    elif request.method == 'POST':
+        return controller_create_admin()
+
+@apiBlueprint.route('/api/admins/<int:admin_id>', methods=['GET', 'PUT', 'DELETE'])
+def manage_admin(admin_id):
+    if request.method == 'GET':
+        return controller_get_admin_by_id(admin_id)
+    elif request.method == 'PUT':
+        return controller_update_admin(admin_id)
+    elif request.method == 'DELETE':
+        return controller_delete_admin(admin_id)
