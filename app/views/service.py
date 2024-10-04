40015da900aaf9dee9abffdbd1a02b358293ccec
alie <mushyafaali@gmail.com>
2024-10-04 16:53:34 +0700

    add servie view

---
+++ app/views/service.py
@@ -0,0 +1,18 @@
+from app.views import apiBlueprint
+from app.controllers.service import *
+
+@apiBlueprint.route('/api/services', methods=['GET', 'POST'])
+def manage_services():
+    if request.method == 'GET':
+        return controller_get_services()
+    elif request.method == 'POST':
+        return controller_create_service()
+
+@apiBlueprint.route('/api/services/<int:service_id>', methods=['GET', 'PUT', 'DELETE'])
+def manage_service(service_id):
+    if request.method == 'GET':
+        return controller_get_service_by_id(service_id)
+    elif request.method == 'PUT':
+        return controller_update_service(service_id)
+    elif request.method == 'DELETE':
+        return controller_delete_service(service_id)
