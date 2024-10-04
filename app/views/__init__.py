59a5f71ec3cf2ffe801dee505884fe660b4d0643
alie <mushyafaali@gmail.com>
2024-10-04 16:57:28 +0700

    add views on init

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -3,6 +3,10 @@ from flask import Blueprint
 apiBlueprint = Blueprint('api', __name__)
 
 from app.views.admin import *
+from app.views.customer import *
+from app.views.service import *
+from app.views.service_professional import *
+
 
 def register_blueprint_views(app):
     app.register_blueprint(apiBlueprint)
