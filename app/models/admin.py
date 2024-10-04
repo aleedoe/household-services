1bdbbfe45d99acd04adcee2b0609275eaec43bd3
alie <mushyafaali@gmail.com>
2024-10-04 15:35:24 +0700

    add security on admin model

--- app/models/admin.py
+++ app/models/admin.py
@@ -1,4 +1,5 @@
 from . import db, datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Admin(db.Model):
     __tablename__ = 'admin'
@@ -7,6 +8,12 @@ class Admin(db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
     
     def __repr__(self):
         return f'<Admin {self.username}>'
