94f6c18d7c7771b6d47a5b996c18565c9da876b1
alie <mushyafaali@gmail.com>
2024-10-04 16:56:01 +0700

    add service professional views

---
+++ app/views/service_professional.py
@@ -0,0 +1,18 @@
+from app.views import apiBlueprint
+from app.controllers.service_professional import *
+
+@apiBlueprint.route('/api/service-professionals', methods=['GET', 'POST'])
+def manage_service_professionals():
+    if request.method == 'GET':
+        return controller_get_service_professionals()
+    elif request.method == 'POST':
+        return controller_create_service_professional()
+
+@apiBlueprint.route('/api/service-professionals/<int:professional_id>', methods=['GET', 'PUT', 'DELETE'])
+def manage_service_professional(professional_id):
+    if request.method == 'GET':
+        return controller_get_service_professional_by_id(professional_id)
+    elif request.method == 'PUT':
+        return controller_update_service_professional(professional_id)
+    elif request.method == 'DELETE':
+        return controller_delete_service_professional(professional_id)
