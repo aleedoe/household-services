be958ebb68dd93bcb89009f3401b7f52f8bb1717
alie <mushyafaali@gmail.com>
2024-10-05 11:25:53 +0700

    add services admin page

--- app/routes/admin/__init__.py
+++ app/routes/admin/__init__.py
@@ -3,3 +3,8 @@ from app.routes import pageBlueprint, render_template
 @pageBlueprint.route('/admin/')
 def admin_test():
     return render_template('admin/test.html')
+
+
+@pageBlueprint.route('/admin/services')
+def admin_services():
+    return render_template('admin/services.html')
