66b1184663f875ed4a80235ba0c5ccab750f3cca
alie <mushyafaali@gmail.com>
2024-10-09 16:54:49 +0700

    add ass page

--- app/routes/service_professional/__init__.py
+++ app/routes/service_professional/__init__.py
@@ -7,3 +7,7 @@ def service_professional_test():
 @pageBlueprint.route('/service-professionals/service-request/')
 def service_professional_request():
     return render_template('service_professional/req_service_professional.html')
+
+@pageBlueprint.route('/service-professionals/service-assigned/')
+def service_professional_assigned():
+    return render_template('service_professional/ass_service_professional.html')
