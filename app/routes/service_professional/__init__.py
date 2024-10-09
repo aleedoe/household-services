6ab0bd3cb1925fedf5e65fbd6ce3f8ea5c01b5e0
alie <mushyafaali@gmail.com>
2024-10-09 13:29:44 +0700

    add req service page

--- app/routes/service_professional/__init__.py
+++ app/routes/service_professional/__init__.py
@@ -3,3 +3,7 @@ from app.routes import pageBlueprint, render_template
 @pageBlueprint.route('/service-professionals/')
 def service_professional_test():
     return render_template('service_professional/test.html')
+
+@pageBlueprint.route('/service-professionals/service-request/')
+def service_professional_request():
+    return render_template('service_professional/req_service_professional.html')
