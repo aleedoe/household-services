6ee302ee7ad356619fe133fd9088306a093e2120
alie <mushyafaali@gmail.com>
2024-10-12 06:04:55 +0700

    add home

--- app/routes/test.py
+++ app/routes/test.py
@@ -3,3 +3,8 @@ from . import pageBlueprint, render_template
 @pageBlueprint.route('/')
 def test():
     return render_template('test.html', title='Test Page')
+
+@pageBlueprint.route('/home')
+def home():
+    return render_template('home.html', title='Home Page')
+
