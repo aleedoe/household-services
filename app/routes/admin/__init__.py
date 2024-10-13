ca22d9933df409841cf889f4b3a0eaba299254d1
alie <mushyafaali@gmail.com>
2024-10-13 10:39:55 +0700

    add auth on all admin page

--- app/routes/admin/__init__.py
+++ app/routes/admin/__init__.py
@@ -1,26 +1,46 @@
+from flask import render_template, redirect, url_for
+
 from app.routes import pageBlueprint, render_template
+from app.routes.auth import session
 
 @pageBlueprint.route('/admin/')
 def admin_test():
-    return render_template('admin/test.html')
-
+    if 'is_logged_in' in session:
+        return render_template('admin/test.html')
+    else:
+        return redirect(url_for('pages.login_admin'))
 
 @pageBlueprint.route('/admin/services')
 def admin_services():
-    return render_template('admin/services.html')
+    if 'is_logged_in' in session:
+        return render_template('admin/services.html')
+    else:
+        return redirect(url_for('pages.login_admin'))
 
 @pageBlueprint.route('/admin/customers')
 def admin_customers():
-    return render_template('admin/customers.html')
+    if 'is_logged_in' in session:
+        return render_template('admin/customers.html')
+    else:
+        return redirect(url_for('pages.login_admin'))
 
 @pageBlueprint.route('/admin/service-professionals')
 def admin_service_professionals():
-    return render_template('admin/service_professional.html')
+    if 'is_logged_in' in session:
+        return render_template('admin/service_professional.html')
+    else:
+        return redirect(url_for('pages.login_admin'))
 
 @pageBlueprint.route('/admin/admins')
 def admin_service_admins():
-    return render_template('admin/admins.html')
+    if 'is_logged_in' in session:
+        return render_template('admin/admins.html')
+    else:
+        return redirect(url_for('pages.login_admin'))
 
 @pageBlueprint.route('/admin/req-services')
 def admin_req_service_admins():
-    return render_template('admin/req_services.html')
+    if 'is_logged_in' in session:
+        return render_template('admin/req_services.html')
+    else:
+        return redirect(url_for('pages.login_admin'))
