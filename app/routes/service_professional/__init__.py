5cad680240013c57d478e041435a85f35ecdb5cf
alie <mushyafaali@gmail.com>
2024-10-13 10:51:36 +0700

    add auth on all service professional  page

--- app/routes/service_professional/__init__.py
+++ app/routes/service_professional/__init__.py
@@ -1,17 +1,33 @@
+from flask import render_template, redirect, url_for
+
 from app.routes import pageBlueprint, render_template
+from app.routes.auth import session
+
 
 @pageBlueprint.route('/service-professionals/')
 def service_professional_test():
-    return render_template('service_professional/test.html')
+    if 'is_logged_in' in session:
+        return render_template('service_professional/test.html')
+    else:
+        return redirect(url_for('pages.login_customer'))
 
 @pageBlueprint.route('/service-professionals/service-request/')
 def service_professional_request():
-    return render_template('service_professional/req_service_professional.html')
+    if 'is_logged_in' in session:
+        return render_template('service_professional/req_service_professional.html')
+    else:
+        return redirect(url_for('pages.login_customer'))
 
 @pageBlueprint.route('/service-professionals/service-assigned/')
 def service_professional_assigned():
-    return render_template('service_professional/ass_service_professional.html')
+    if 'is_logged_in' in session:
+        return render_template('service_professional/ass_service_professional.html')
+    else:
+        return redirect(url_for('pages.login_customer'))
 
 @pageBlueprint.route('/service-professionals/service-review/')
 def service_professional_review():
-    return render_template('service_professional/review_service_professional.html')
+    if 'is_logged_in' in session:
+        return render_template('service_professional/review_service_professional.html')
+    else:
+        return redirect(url_for('pages.login_customer'))
